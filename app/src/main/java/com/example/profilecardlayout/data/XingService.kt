e20407db8a27363f9a41506fe5e5a10ae4f06f17
João Fonte <joao.fonte@nb-jfonte.home>
2021-11-15 15:48:10 +0000

    correcting things

--- app/src/main/java/com/example/profilecardlayout/data/XingService.kt
+++ app/src/main/java/com/example/profilecardlayout/data/XingService.kt
@@ -5,7 +5,7 @@ import com.example.profilecardlayout.domain.RepositoryModel
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
-class XingService @Inject constructor(private val api: XingApi)  : GetReposRepository {
+class XingService @Inject constructor(private val api: XingApi) : GetReposRepository {
 
     override fun getRepos(): Single<List<RepositoryModel>> {
         return api.getRepos().map { it.toDomainModel() }
